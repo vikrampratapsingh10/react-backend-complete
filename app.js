05e1b5e7de075e87582dadb097d60601ac950a77
vikrampratapsingh10 <124668584+vikrampratapsingh10@users.noreply.github.com>
2023-06-07 12:49:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/",(req,res)=>{
   res.setHeader("Access-Control-Allow-Credentials",true);
-  res.send("API is running...")
+  res.setHeader("Access-Control-Allow-Origin: *");
 });
 app.use("/customer", CustomerRouter);
 app.use("/product", ProductRouter);
