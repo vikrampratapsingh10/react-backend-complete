27037eb9f6222dde2a44ab149629cff768dffa52
vikrampratapsingh10 <124668584+vikrampratapsingh10@users.noreply.github.com>
2023-06-07 12:41:43 +0530

    app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get("/",(req,res,next)=>{
+app.get("/",(req,res)=>{
   res.setHeader("Access-Control-Allow-Credentials",true);
   res.send("API is running...")
 });
